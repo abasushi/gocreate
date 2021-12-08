c6181a35cc7d17b7c089eb1000e9a51b39db73e2
Sean David Abalora <seanabalora@gmail.com>
2021-12-08 12:05:00 +0800

    GoCreate
    Prototype Design

---
+++ resources/login-register/login-signup-animations.js
@@ -0,0 +1,7 @@
+/*===== GSAP ANIMATION =====*/
+// NAV
+gsap.from('.logo', {opacity: 0, duration: 0.5, delay: 1, y: -15})
+
+// FORM
+gsap.from('.login-signup-container', {opacity: 0, duration: 0.5, delay: 1, y: 15})
+gsap.from('.overlay-design-container', {opacity: 0, duration: 0.5, delay: 1, y: 15})
