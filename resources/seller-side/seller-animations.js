0e4041f2c79c49265857d059ec7ea2ed5315f30b
Sean David Abalora <seanabalora@gmail.com>
2021-12-09 16:25:54 +0800

    GoCreate
    Prototype 4.0

---
+++ resources/seller-side/seller-animations.js
@@ -0,0 +1,3 @@
+// PAGE
+gsap.from('.profile-section', {opacity: 0, duration: 0.5, delay: 1, y: 15})
+gsap.from('.navigation-section', {opacity: 0, duration: 0.5, delay: 1, y: 15})
--- resources/seller-side/seller-script.js
+++ resources/seller-side/seller-script.js
@@ -25,15 +25,3 @@ function move(e){
         layer.style.transform = `translateX(${x}px) translateY(${y}px)`
     })
 }
-
-/*===== GSAP ANIMATION =====*/
-// NAV
-gsap.from('.logo', {opacity: 0, duration: 0.5, delay: 1, y: -15})
-gsap.from('.nav-item', {opacity: 0, duration: 0.5, delay: 1, y: -15, stagger: 0,})
-gsap.from('.nav-toggle-btn', {opacity: 0, duration: 0.5, delay: 1, y: -15, stagger: 0,})
-
-// HOME
-gsap.from('.title', {opacity: 0, duration: 0.5, delay: 1, y: 15})
-gsap.from('.text', {opacity: 0, duration: 0.5, delay: 1, y: 15})
-gsap.from('.join-btn', {opacity: 0, duration: 0.5, delay: 1, y: 15})
-gsap.from('.creatives', {opacity: 0, duration: 0.5, delay: 1, y: 15})
